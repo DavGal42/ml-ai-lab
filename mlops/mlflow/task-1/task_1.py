13d919e7d3c3f438ca312524288124265a6a3c99
David Galstyan <galstyand.25@gmail.com>
2026-02-02 17:24:58 +0400

    Update .gitignore and requirements; Add first MLFLow task

---
+++ mlops/mlflow/task-1/task_1.py
@@ -0,0 +1,29 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+X, y = fetch_california_housing(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+mlflow.set_experiment("California_linear_regression")
+
+with mlflow.start_run():
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+
+    mlflow.log_param("model_type", "LinearRegression")
+    mlflow.log_param("fit_intercept", model.fit_intercept)
+
+    mlflow.log_metric("mse", mse)
+
+    mlflow.sklearn.log_model(model, "linear_model")
+
+    print(f"Run finished. MSE: {mse}")
